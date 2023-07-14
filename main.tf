31a18efacba945881865b38910c49270e0504cb2
upendra <ugrarapuupendra@gmail.com>
2023-07-14 10:22:21 +0530

    updated code

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_spot_instance_request" "rabbitmq" {
+/*resource "aws_spot_instance_request" "rabbitmq" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
   subnet_id = var.subnet_ids[0]
@@ -27,6 +27,38 @@ resource "aws_route53_record" "main" {
   type    = "A"
   ttl     = 30
   records = [aws_spot_instance_request.rabbitmq.private_ip]
+}*/
+
+
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  subnet_id = var.subnet_ids[0]
+  wait_for_fulfillment = true
+  vpc_security_group_ids = [aws_security_group.main.id]
+  iam_instance_profile = aws_iam_instance_profile.main.name
+
+  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
+    component = "rabbitmq"
+    env       = var.env
+  }))
+
+  tags = merge(
+    var.tags,
+    {Name = "${var.env}-rabbitmq"}
+  )
+}
+resource "aws_ec2_tag" "name-tag" {
+  key         = "Name"
+  resource_id = aws_instance.rabbitmq.private_ip
+  value       = "rabbitmq-${var.env}"
+}
+resource "aws_route53_record" "main" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = "rabbitmq-${var.env}.${var.dns_domain}"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
 }
 
 resource "aws_security_group" "main" {
